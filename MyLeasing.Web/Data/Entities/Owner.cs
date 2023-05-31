b3c2044c1c50dc82e047a4b3a581f203decdf437
Diogoepronto <bta.diogo@gmail.com>
2023-05-31 23:56:17 +0100

    CRUD Owners

---
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int Document { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+        [Display(Name = "Cell Phone")]
+        public string CellPhone { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
